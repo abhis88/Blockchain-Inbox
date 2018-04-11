b25eb33d2cf28a1d13c0a73ab84e08a97fcb1d24
abhishek-nyllabs <abhishek@nyllabs.tech>
2018-04-11 13:32:50 -0400

    Added Mocha tests

--- compile.js
+++ compile.js
@@ -5,5 +5,7 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-//console.log(solc.compile(source,1));
-module.exports = solc.compile(source,1).contracts[':Inbox'];
+//console.log(solc.compile(source,1)); // no of contracts to be compiled is 1
+
+//outputs a json object with interface and bytecode
+module.exports = solc.compile(source,1).contracts[':Inbox']; 
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,12 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+//const web3 = new Web3(ganache.provider()); //Older version has this
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+//module export object catch
 const {interface, bytecode} = require('../compile');
 
 let accounts;
@@ -16,12 +21,23 @@ beforeEach(async () => {
     inbox = await new web3.eth.Contract(JSON.parse(interface))
                         .deploy({data: bytecode, arguments: ['Hi there!']})
                         .send({from: accounts[0], gas: '1000000'});
-
+    inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
     it('deploys a contract to local ganache', () => {
-        console.log(inbox);
+        //console.log(inbox);
+        assert.ok(inbox.options.address);
+    });
+    
+    it('has a default message assinged', async() => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi there!');
+    });
 
+    it('can change the message', async() => {
+        await inbox.methods.setMessage('New Message').send({from: accounts[0]});
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'New Message');
     });
 })
